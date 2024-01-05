76f0ab6b9eab0d09224086243907e93bd6244faf
Dilip <dilip@ubuntu.myguest.virtualbox.org>
2024-01-05 11:57:54 +0530

    new file

--- soucre_code/index.php
+++ soucre_code/index.php
@@ -65,7 +65,7 @@
 
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $servername = "10.43.74.179";
+    $servername = "10.43.74.179"; 
     $username = "root";
     $password = "root-password";
     $dbname = "sampledb";
